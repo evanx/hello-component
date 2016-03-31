c6a9326f46a92d1f7edc4d2a426c583ec8f168ad
Evan Summers <evan.summers@gmail.com>
2016-03-31 12:29:39 +0200

    logging

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ export default async function(state, props, logger, metrics, service) {
    logger.info('hello', props);
    return {
       async start() {
-         logger.info('state ready');
+         logger.info('system ready');
       },
       async end() {
          logger.info('goodbye');
